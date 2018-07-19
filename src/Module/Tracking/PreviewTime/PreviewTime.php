4497aa4c29badb858ce1bbe5da60903c090227b8
maik.schoessler <maik.schoessler@check24.de>
2018-07-19 20:52:56 +0200

    - removed unused importCsvAction

---
+++ src/Module/Tracking/PreviewTime/PreviewTime.php
@@ -0,0 +1,108 @@
+<?php
+declare(strict_types=1);
+
+namespace Project\Module\Tracking\PreviewTime;
+
+use Project\Module\GenericValueObject\Date;
+use Project\Module\GenericValueObject\Distance;
+use Project\Module\GenericValueObject\Id;
+use Project\Module\Tracking\RoundTime;
+
+/**
+ * Class PreviewTime
+ * @package Project\Module\Tracking\PreviewTime
+ */
+class PreviewTime
+{
+    /** @var Id $previewTimeId */
+    protected $previewTimeId;
+
+    /** @var Id $competitionId */
+    protected $competitionId;
+
+    /** @var Date $start */
+    protected $start;
+
+    /** @var Date $end */
+    protected $end;
+
+    /** @var RoundTime $roundTime */
+    protected $roundTime;
+
+    /** @var Distance $distance */
+    protected $distance;
+
+    /**
+     * @return Id
+     */
+    public function getPreviewTimeId(): Id
+    {
+        return $this->previewTimeId;
+    }
+
+    /**
+     * @return Id
+     */
+    public function getCompetitionId(): Id
+    {
+        return $this->competitionId;
+    }
+
+    /**
+     * @return Date
+     */
+    public function getStart(): Date
+    {
+        return $this->start;
+    }
+
+    /**
+     * @return Date
+     */
+    public function getEnd(): Date
+    {
+        return $this->end;
+    }
+
+    /**
+     * @return RoundTime
+     */
+    public function getRoundTime(): RoundTime
+    {
+        return $this->roundTime;
+    }
+
+    /**
+     * @return Distance
+     */
+    public function getDistance(): Distance
+    {
+        return $this->distance;
+    }
+
+    /**
+     * PreviewTime constructor.
+     *
+     * @param Id $previewTimeId
+     * @param Id $competitionId
+     * @param Date $start
+     * @param Date $end
+     * @param Distance $distance
+     */
+    public function __construct(Id $previewTimeId, Id $competitionId, Date $start, Date $end, Distance $distance)
+    {
+        $this->previewTimeId = $previewTimeId;
+        $this->competitionId = $competitionId;
+        $this->start = $start;
+        $this->end = $end;
+        $this->distance = $distance;
+    }
+
+    /**
+     * @param RoundTime $roundTime
+     */
+    public function setRoundTime(RoundTime $roundTime): void
+    {
+        $this->roundTime = $roundTime;
+    }
+}
---
+++ src/Module/Tracking/Round/Round.php
@@ -0,0 +1,75 @@
+<?php
+declare(strict_types=1);
+
+namespace Project\Module\Tracking\Round;
+
+/**
+ * Class Round
+ * @package Project\Module\Tracking\Round
+ */
+class Round
+{
+    /** @var int $round */
+    protected $round;
+
+    /**
+     * Round constructor.
+     *
+     * @param int $round
+     */
+    protected function __construct(int $round)
+    {
+        $this->round = $round;
+    }
+
+    /**
+     * @param $round
+     *
+     * @return Round
+     * @throws \InvalidArgumentException
+     */
+    public static function fromValue($round): self
+    {
+        self::ensureRoundIsValid($round);
+
+        return new self(self::convertRound($round));
+    }
+
+    /**
+     * @param $round
+     *
+     * @throws \InvalidArgumentException
+     */
+    protected static function ensureRoundIsValid($round): void
+    {
+        if ((\is_int($round) === false && \is_string($round) === false) || (int)$round < 0) {
+            throw new \InvalidArgumentException('Round is not valid: ' . $round);
+        }
+    }
+
+    /**
+     * @param $round
+     *
+     * @return int
+     */
+    protected static function convertRound($round): int
+    {
+        return (int)$round;
+    }
+
+    /**
+     * @return int
+     */
+    public function getRound(): int
+    {
+        return $this->round;
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return (string)$this->getRound();
+    }
+}

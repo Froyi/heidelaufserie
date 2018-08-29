13553d7472f6628abe83c5e79be7605ae1e9c782
Maik Schößler <maik.schoessler@check24.de>
2018-08-29 07:10:39 +0200

    ENV config add
    - add an environment config, mainly for databases

---
+++ config-env.php
@@ -0,0 +1,12 @@
+<?php
+
+use Project\ConfigurationInterface;
+
+return [
+    'database' => [
+        'host' => ConfigurationInterface::DEFAULT_SERVER,
+        ConfigurationInterface::USER => 'root',
+        ConfigurationInterface::PASS => '',
+        'database_name' => 'heidelaufserie',
+    ],
+];
